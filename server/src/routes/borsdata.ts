f9a770aa1b55263c8f3c1432dd8fb01e037429a8
jacobkarlen <jacka782@student.liu.se>
2022-04-05 17:34:53 +0200

    feat: refactor method for getting data from 3rd-party borsdata api and add additional rotues for markets, countries, sectors and branches

--- server/src/routes/borsdata.ts
+++ server/src/routes/borsdata.ts
@@ -7,24 +7,39 @@ export const borsdataRouter = Router();
 
 const API_BASE_URI = 'https://apiservice.borsdata.se/v1'
 
-borsdataRouter.get('/borsdata/markets', async (req: Request, res: Response) => {
+const getBorsdataData = (path: string, res: Response) => {
     const API_KEY = process.env.BORSDATA_API_KEY
 
-    https.get(`${API_BASE_URI}/markets?authKey=${API_KEY}`, (b_res: any) => {
+    https.get(`${API_BASE_URI}${path}?authKey=${API_KEY}`, (b_res: any) => {
 
-        console.log(`${API_BASE_URI}/markets?authKey=${API_KEY}`)
         let data = ''
         b_res.on('data', (chunk: string) => {
             data += chunk
         })
 
         b_res.on('end', () => {
-           res.json(JSON.parse(data))
+            return res.json(JSON.parse(data))
         })
         
     }).on('error', (error: Error) => {
         console.log("Error: ", error)
-        res.json({ message: "An error occured on the server"})
+        return res.json({ message: "An error occured on the server while requesting borsdata api"})
     })
+}
 
+borsdataRouter.get('/borsdata/markets', async (req: Request, res: Response) => {
+   getBorsdataData('/markets', res)
 })
+
+borsdataRouter.get('/borsdata/countries', async (req: Request, res: Response) => {
+    getBorsdataData('/countries', res)
+ })
+
+ borsdataRouter.get('/borsdata/branches', async (req: Request, res: Response) => {
+    getBorsdataData('/branches', res)
+ })
+
+ 
+ borsdataRouter.get('/borsdata/sectors', async (req: Request, res: Response) => {
+    getBorsdataData('/sectors', res)
+ })
